71e50cb558e72f519403dd53f81fa7ca3c98ea8f
Okoromi Victor <okoromivictorsunday@gmail.com>
2020-08-29 21:24:51 +0100

    updated commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 title="This is react">React for show</h1>
+    </div>
+  );
+}
+
+export default App;
